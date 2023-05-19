11fc12ec42e1f4333496afef4eadf84033668340
jpalma-duque <jopalma@uc.cl>
2023-05-19 13:46:20 -0400

    añadimos mapa a la visualizacion

--- aplicacion/.ipynb_checkpoints/app-checkpoint.py
+++ aplicacion/.ipynb_checkpoints/app-checkpoint.py
@@ -1,55 +1,49 @@
 import streamlit as st
-import plotly.graph_objects as go
-
+import pandas as pd
+import plotly.express as px
 
 
 def main():
-    st.title('Resultados interesantisimos')
-
-    boton = st.button('Botoncito')
-
-    st.write(boton)
-
-    option = st.selectbox(
-        'How would you like to be contacted?',
-        ('Email', 'Home phone', 'Mobile phone'))
-
-    st.write('You selected:', option)
     
-    # Plot the figure
-    fig = plot_data()
-    st.plotly_chart(fig)
+    st.title('Visualizar resultados API')
+    
+    df = leer_archivos()
 
+    especies = df.species.unique()
+    especie = st.selectbox('Selecciona una especie', ['Todas las especies'] + list(especies), index=1)
+    
+    mapa, datos = st.tabs(['Mapa', 'Base de datos'])
+    
+    if especie == 'Todas las especies':
+        sub_df = df.copy()
+    else:
+        sub_df = df.query('species==@especie').copy()
+    
+    with datos:
+        st.write(sub_df)
+    
+    with mapa:
+        fig = graficar_especies(sub_df)
+        st.plotly_chart(fig)
+    
+    
+    
+def graficar_especies(df):
 
+    fig = px.scatter_mapbox(df, lat="decimalLatitude", lon="decimalLongitude", hover_data=["species"],
+                            color="species", zoom=5, height=600, width=800)
 
-
-def plot_data():
-    # Sample data
-    timestamps = ['2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04', '2023-01-05']
-    values = [10, 15, 7, 12, 9]
-
-    # Create a trace
-    trace = go.Scatter(
-        x=timestamps,
-        y=values,
-        mode='lines',
-        name='Time Series Data'
-    )
-
-    # Create data array with the trace
-    data = [trace]
-
-    # Create layout
-    layout = go.Layout(
-        title='Time Series Plot',
-        xaxis=dict(title='Timestamp'),
-        yaxis=dict(title='Values')
-    )
-
-    # Create figure
-    fig = go.Figure(data=data, layout=layout)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(mapbox_center={"lat": -53.0, "lon": -70.0})
 
     return fig
+
+    
+    
+@st.cache_data    
+def leer_archivos():
+    df = pd.read_parquet('aplicacion/datos_app/datos_consolidados_observaciones.parquet')
+    return df
 
 
 main()
